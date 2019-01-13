143135e65cfe44bbdae88c063f9b6504fa9d5b97
Sharan Somaskanthan <kingsharan02161999@gmail.com>
2019-01-12 23:01:40 -0500

    sentiment analyzer

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import json
+from sentiment_analyzer import Sentiment_analyzer as s
 import requests
+import praw
 
+text = "Good movie"
 # Make a get request to get the latest position of the international space station from the opennotify api.
-response = requests.post("http://text-processing.com/api/sentiment/", data = {'text':'good movies'})
+response = requests.post("http://text-processing.com/api/sentiment/", data = {'text':text})
+reddit = praw.Reddit(client_id = 'HrXPcMKV26wpNQ',
+					client_secret = '8b1AyC3-CCiD4Cs5TZAKXmYCa-I',
+					user_agent = 'my user agent')
+for submission in reddit.subreddit('apple').hot(limit=2):
+    print(submission.title)
 
-print(response.json())
+y = json.loads(response.text)
+print(y["label"])
---
+++ sentiment_analyzer.py
@@ -0,0 +1,34 @@
+import json
+import requests
+class Sentiment_analyzer:
+	def __init__(self):
+		self.pos = 0
+		self.neg = 0
+		self.neut = 0
+		self.item_count = 0
+		print("class created")
+	
+	def classify(self, text):
+		print("classify")
+		response = requests.post("http://text-processing.com/api/sentiment/", data = {'text':text})
+		print(response.text)
+		self.pos += response.json()["probability"]["pos"]
+		self.neg += response.json()["probability"]["neg"]
+		self.neut += response.json()["probability"]["neutral"]
+		self.item_count+=1
+		print(str(self.pos) + ', ' + str(self.neut) + ', ' + str(self.neg))
+		return [self.pos, self.neut, self.neg]
+	
+	def average(self):
+		return [self.pos/self.item_count, self.neut/self.item_count, self.neg/self.item_count]
+	
+	def evaluate(self):
+		average = self.average()
+		return average.index(max(average))
+
+		
+'''
+s = Sentiment_analyzer()
+array = s.classify("good")
+print(s.evaluate())
+'''
