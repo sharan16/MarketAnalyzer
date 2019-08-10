89a01e654c03c651e0905d147a40fada26616b5f
Sharan Somaskanthan <kingsharan02161999@gmail.com>
2019-08-09 20:11:33 -0400

    made csv and used matplotlib to plot stock data

--- marketanalyzer.py
+++ marketanalyzer.py
@@ -6,9 +6,14 @@ import pandas_datareader.data as web
 
 style.use("ggplot")
 
-start_date = dt.datetime(2000,1,1)
-end_date = dt.datetime(2016,1,1)
+# start_date = dt.datetime(2000,1,1)
+# end_date = dt.datetime(2016,1,1)
 
-df = web.DataReader('AAPL','yahoo',start_date,end_date)
+# df = web.DataReader('AAPL','yahoo',start_date,end_date)
+# df.to_csv('tsla.csv')
+df = pd.read_csv("tsla.csv", parse_dates = True, index_col = 0)
 
 print(df.head())
+
+df['Open'].plot()
+plt.show()
